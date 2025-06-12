2d263ec525cc5562d48f6a2d1d15a395624759f9
Ryan Weast <ryweast@gmail.com>
2025-06-12 22:07:47 +0000

    Implement code to analyze number of service requests associated with Licenses & Inspections

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import requests
 
-r = requests.get(
+# Service Requests
+r1 = requests.get(
     "https://phl.carto.com/api/v2/sql",
-    params={"q": "SELECT * FROM violations WHERE violationdate >=  '2025-01-01T00:00:00Z'"}
+    params={"q": "SELECT (SELECT COUNT(*) FROM public_cases_fc WHERE agency_responsible = 'License & Inspections' AND requested_datetime >= '2025-01-01') as num_cases, (SELECT COUNT(*) FROM public_cases_fc WHERE requested_datetime >= '2025-01-01') as total_cases"}
 )
-print(r.text)
+r1_json = r1.json()
+print(f"Since the beginning of the 2025, {r1_json['rows'][0]['num_cases']} out of {r1_json['rows'][0]['total_cases']} service requests were associated with Licenses & Inspections")
+
+# L&I
+#r = requests.get(
+#    "https://phl.carto.com/api/v2/sql",
+#    params={"q": "SELECT * FROM violations WHERE violationdate >= '2025-01-01T00:00:00Z'"}
+#)
+#print(r.text)
