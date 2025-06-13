2a17f1e80365ea981a96af4e9abccd5ef301b594
Ryan Weast <ryweast@gmail.com>
2025-06-13 18:37:24 +0000

    Improve documentation

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import requests
 import urllib.parse
 import time
 
+print("Script started")
+
 def write_analysis(analysis):
     with open('analysis.txt', 'a+') as f:
+        print(f"Wrote analysis: {analysis}")
         f.write(analysis + "\n\n")
 
-# Service Requests associated with L&I
+# Count of service Requests associated with L&I
 r1 = requests.get(
     "https://phl.carto.com/api/v2/sql",
     params={"q": "SELECT (SELECT COUNT(*) FROM public_cases_fc WHERE agency_responsible = 'License & Inspections' AND requested_datetime >= '2025-01-01') as num_cases, (SELECT COUNT(*) FROM public_cases_fc WHERE requested_datetime >= '2025-01-01') as total_cases"}
@@ -14,7 +17,7 @@ r1 = requests.get(
 r1_json = r1.json()
 write_analysis(f"Since the beginning of 2025, {r1_json['rows'][0]['num_cases']} out of {r1_json['rows'][0]['total_cases']} service requests were associated with Licenses & Inspections")
 
-# 
+# Dump of service requests, to find related violations
 r2 = requests.get(
     "https://phl.carto.com/api/v2/sql",
     params={"q": "SELECT service_request_id, address, requested_datetime FROM public_cases_fc WHERE agency_responsible = 'License & Inspections' AND requested_datetime >= '2025-01-01'"}
@@ -22,20 +25,19 @@ r2 = requests.get(
 service_requests = r2.json()['rows']
 total_requests = 0
 matching_violations = []
-print(len(service_requests))
 for row in service_requests:
     if row['address']:
         # Use urllib.parse.quote to urlencode the address, replacing any special characters with their hex encoding
         opa_lookup = requests.get(f"https://api.phila.gov/ais/v2/search/{urllib.parse.quote(row['address'])}")
         # Some addresses don't seem to have an `opa_account_num`, so we will leave them out of our data analysis
         if opa_lookup.headers['Content-Type'] == 'application/json' and 'features' in opa_lookup.json() and 'opa_account_num' in opa_lookup.json()['features'][0]['properties']:
+            # Keep count of the number of requests that we go through, so we can log progress
             total_requests += 1
             if total_requests % 100 == 0:
                 print(f"Analysis {(100 * total_requests / len(service_requests)):.2f}% done")
             opa_account_num = opa_lookup.json()['features'][0]['properties']['opa_account_num']
-            #print(opa_account_num)
             # Find violations with matching OPA numbers, where casecreatedstart is within one month of the service request date
-            # IDEA: Get all cases up front, so we don't need a request for each one
+            # Performance enhanccement idea: Get all cases up front, so we don't need a request for each one
             violation_lookup = requests.get(
                 "https://phl.carto.com/api/v2/sql",
                 params={"q": f"SELECT casecreateddate, casestatus, violationstatus, casecompleteddate FROM violations WHERE opa_account_num = '{opa_account_num}' AND casecreateddate >= '{row['requested_datetime']}' AND casecreateddate <= ('{row['requested_datetime']}'::date + INTERVAL '1 month')"}
@@ -47,7 +49,7 @@ write_analysis(f"Since the beginning of 2025, {len(matching_violations)} out of 
 
 num_closed_violations = 0
 for matching_violation in matching_violations:
-    # For service requests with multiple matching violations, only report they are completed if all violations are completed
+    # For service requests with multiple matching violations, only report they are completed if all violations are closed
     num_completed = 0
     for v in matching_violation:
         if v['casestatus'] == 'CLOSED':
